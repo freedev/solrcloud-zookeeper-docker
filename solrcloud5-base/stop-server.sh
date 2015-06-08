dcd938ab84deaa46341295814dbc480802edc9d3
Vincenzo D'Amore <v.damore@gmail.com>
2015-06-08 17:49:38 +0200

    fixed solrcloud5 image build

--- solrcloud5-base/stop-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-# ps -ef | grep java | awk '{ print $2 }' | xargs kill
-
-/opt/solr-5.1.0/bin/solr stop -p 8080 -all
