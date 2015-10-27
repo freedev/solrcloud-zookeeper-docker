c57e8df332ade0b3848baedd552b1534ffd6f7af
Vincenzo D'Amore <v.damore@gmail.com>
2015-10-27 18:16:03 +0100

    fixed solrcloud4 configuration

--- solrcloud4/start-server.sh
+++ solrcloud4/start-server.sh
@@ -2,7 +2,17 @@
 
 if [ ! -f $SOLR_DATA/solr.xml ]
 then
-	cp /opt/solr-config/solr.xml $SOLR_DATA/
+	cp /opt/config/solr.xml $SOLR_DATA/
 fi
+
+cp /etc/hosts /opt/config/hosts
+
+echo 'Waiting for hosts file to appear...'
+while [ ! -f /opt/config/hosts.cluster ] ; do
+        sleep 1
+done
+echo 'hosts file found, starting server.'
+cat /opt/config/hosts /opt/config/hosts.cluster > /etc/hosts
+rm /opt/config/hosts.cluster
 
 /opt/tomcat/bin/catalina.sh run >> $SOLR_LOG_DIR/solr-tomcat-console.log 2>&1
