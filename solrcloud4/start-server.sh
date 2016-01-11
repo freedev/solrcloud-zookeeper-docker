bf50142ce186268890970bd8edddc70f8f023f88
Vincenzo D'Amore <v.damore@gmail.com>
2016-01-11 22:31:48 +0100

    fixed solrcloud4 config

--- solrcloud4/start-server.sh
+++ solrcloud4/start-server.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+if [ -z "$SOLR_LOG_DIR" ]
+then
+        SOLR_LOG_DIR=/var/log/
+fi
+
+if [ ! -d $SOLR_DATA ]
+then
+        echo "ERROR: " $SOLR_DATA " missing..." 
+        echo "ERROR: " $SOLR_DATA " missing..." >> $SOLR_LOG_DIR/solr.log
+        exit
+fi
+
+# If solr.xml does not exist create a default one
 if [ ! -f $SOLR_DATA/solr.xml ]
 then
 	cp /opt/config/solr.xml $SOLR_DATA/
+fi
+
+# Create lib directory for solrcloud customizations
+if [ ! -d $SOLR_DATA/lib ]
+then
+	mkdir $SOLR_DATA/lib
 fi
 
 cp /etc/hosts /opt/config/hosts
@@ -12,10 +31,11 @@ while [ ! -f /opt/config/hosts.cluster ] ; do
         sleep 1
 done
 echo 'hosts file found, starting server.'
+
 cat /opt/config/hosts /opt/config/hosts.cluster > /etc/hosts
 rm /opt/config/hosts.cluster
 
-/opt/tomcat/bin/catalina.sh start >> $SOLR_LOG_DIR/solr-tomcat-console.log 2>&1
+exec /opt/tomcat/bin/catalina.sh start >> $SOLR_LOG_DIR/solr-tomcat-console.log 2>&1
 
 while [ ! -f /opt/config/stop.node ] ; do
         sleep 1
