48f40958db99ad4e4a7d3fbbd36950e5cafa66e5
Vincenzo D'Amore <v.damore@gmail.com>
2016-10-25 15:24:47 +0200

    fixing start & stop script for mac os x  docker

--- start.sh
+++ start.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+function my_readlink() {
+  case "$OSTYPE" in
+    solaris*) echo "SOLARIS" ;;
+    darwin*)
+       echo $( cd "$1" ; pwd -P )
+       ;;
+    linux*)
+       echo $(readlink -f $1)
+        ;;
+    bsd*)     echo "BSD" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+  esac
+}
+
 PWD=$(pwd)
-PWD_PATH=$(readlink -f $PWD)
-SCRIPT_PATH=$(readlink -f $0)
-SCRIPT_NAME=$(basename $0)
+PWD_PATH=$(my_readlink $PWD)
+SCRIPT_PATH=$(my_readlink $(dirname "$0"))
+SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
-if [ "$SCRIPT_DIR" == "$PWD" ]
+if [ "$SCRIPT_PATH" == "$PWD" ]
 then
-	export SZD_HOME="$SCRIPT_DIR"
+	export SZD_HOME="$SCRIPT_PATH"
 	export SOLRCLOUD_CLUSTER_SIZE=1
-	$SZD_HOME/sbin/common.sh
-	$SZD_HOME/sbin/start-zookeeper-standalone.sh
-	$SZD_HOME/sbin/start-solrcloud5-cluster.sh
+	bash -x $SZD_HOME/sbin/common.sh
+	bash -x $SZD_HOME/sbin/start-zookeeper-standalone.sh
+	bash -x $SZD_HOME/sbin/start-solrcloud5-cluster.sh
 else
 	echo ""
 	echo "execute:"
 	echo ""
-	echo "  cd "$SCRIPT_DIR
+	echo "  cd "$SCRIPT_PATH
 	echo "  ./"$SCRIPT_NAME
 	echo ""
 fi
--- stop.sh
+++ stop.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+function my_readlink() {
+  case "$OSTYPE" in
+    solaris*) echo "SOLARIS" ;;
+    darwin*)
+       echo $( cd "$1" ; pwd -P )
+       ;;
+    linux*)
+       echo $(readlink -f $1)
+        ;;
+    bsd*)     echo "BSD" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+  esac
+}
+
 PWD=$(pwd)
-PWD_PATH=$(readlink -f $PWD)
-SCRIPT_PATH=$(readlink -f $0)
-SCRIPT_NAME=$(basename $0)
+PWD_PATH=$(my_readlink $PWD)
+SCRIPT_PATH=$(my_readlink $(dirname "$0"))
+SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
-if [ "$SCRIPT_DIR" == "$PWD" ]
+if [ "$SCRIPT_PATH" == "$PWD" ]
 then
-	export SZD_HOME="$SCRIPT_DIR"
+	export SZD_HOME="$SCRIPT_PATH"
 	export SOLRCLOUD_CLUSTER_SIZE=1
 	$SZD_HOME/sbin/common.sh
 	$SZD_HOME/sbin/stop-all.sh
@@ -18,7 +32,7 @@ else
 	echo ""
 	echo "execute:"
 	echo ""
-	echo "  cd "$SCRIPT_DIR
+	echo "  cd "$SCRIPT_PATH
 	echo "  ./"$SCRIPT_NAME
 	echo ""
 fi
