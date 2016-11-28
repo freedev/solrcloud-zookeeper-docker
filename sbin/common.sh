733c41861f2a6d9f93f5d1b9bbd4de26c5049f0c
Vincenzo D'Amore <v.damore@gmail.com>
2016-11-28 14:59:51 +0100

    removed old scripts

--- sbin/common.sh
+++ sbin/common.sh
@@ -4,42 +4,15 @@ set -e
 
 [ -z "$SZD_HOME" ] && echo "ERROR: "\$SZD_HOME" environment variable not found!" && exit 1;
 
-if [ -z "$SZD_DATA_DIR" ]
-then
-	export SZD_DATA_DIR=$SZD_HOME/data
-	if [ ! -d $SZD_HOME/data ]
-	then
-		mkdir $SZD_HOME/data
-	fi
-fi
-
-if [ ! -d $SZD_DATA_DIR ]
-then
-	echo "ERROR: "$SZD_DATA_DIR" unable to access contaners data dir!"
-	exit 1
-fi
-
-export SZD_CONFIG_DIR=$SZD_DATA_DIR/config
-
-if [ ! -d $SZD_CONFIG_DIR ]
-then
-	echo "INFO: "$SZD_CONFIG_DIR" not found, creating..."
-	mkdir -p $SZD_CONFIG_DIR
-fi
-
 export DOCKER_BIN="sudo docker"
 
 export DOCKER_COMPOSE_BIN="sudo docker-compose"
 
-export ZK_CLUSTER_SIZE=${ZK_CLUSTER_SIZE:-3}
+# export ZK_JVMFLAGS="-Xms512m -Xmx2048m"
 
-export ZK_JVMFLAGS="-Xms512m -Xmx2048m"
+# export ZKHOST_CFG_FILE=$SZD_CONFIG_DIR/zkhost.cfg
 
-export ZKHOST_CFG_FILE=$SZD_CONFIG_DIR/zkhost.cfg
-
-export SOLRCLOUD_CLUSTER_SIZE=${SOLRCLOUD_CLUSTER_SIZE:-3}
-
-export SOLRCLOUD_JVMFLAGS=${SOLRCLOUD_JVMFLAGS:-"-Xms512m -Xmx2048m"}
+# export SOLRCLOUD_JVMFLAGS=${SOLRCLOUD_JVMFLAGS:-"-Xms512m -Xmx2048m"}
 
 export SZD_COMMON_CONFIG="LOADED"
 
--- sbin/force-shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$DOCKER_BIN ps -q | xargs -I{} $DOCKER_BIN exec -i {} /kill-server.sh
-sleep 2
-$DOCKER_BIN ps -q | xargs -I{} $DOCKER_BIN stop {} 
-sleep 2
-$DOCKER_BIN ps -a -q | xargs -I{} $DOCKER_BIN rm {}
-
-
