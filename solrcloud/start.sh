7738f4cc7153ca904c93de9f4415fabe547e34d5
Vincenzo D'Amore <v.damore@gmail.com>
2017-02-13 23:34:32 +0100

    fixed logs and data ownership

--- solrcloud/start.sh
+++ solrcloud/start.sh
@@ -102,6 +102,7 @@ for ((i=1; i <= SOLRCLOUD_CLUSTER_SIZE ; i++)); do
     mkdir -p ${HOST_DATA_DIR}/store/shared-lib
     cp -r $SZD_HOME/templates/solr/docker-entrypoint-initdb.d ${HOST_DATA_DIR}/
     cp $SZD_HOME/templates/solr/solr.xml ${HOST_DATA_DIR}/store/solr/solr.xml
+    chmod -R ugo+rw ${HOST_DATA_DIR}
   fi
 
   if [ ! -d ${HOST_DATA_DIR} ] ; then
@@ -115,8 +116,6 @@ for ((i=1; i <= SOLRCLOUD_CLUSTER_SIZE ; i++)); do
   fi
 
 done
-
-chmod -R ugo+rw ${SZD_DATA_DIR}
 
 echo
 echo -n "Waiting for cluster and ensemble startup... "
